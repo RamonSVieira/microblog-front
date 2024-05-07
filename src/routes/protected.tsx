15baba146b2782291912177a44e31decd5b3613e
RamonSVieira <ramonvieira122333@gmail.com>
2024-05-07 14:14:37 -0300

    feat: tela de cadastro e registro na api (#2)

--- src/routes/protected.tsx
+++ src/routes/protected.tsx
@@ -3,8 +3,6 @@ import { useAuth } from "../context/AuthProvider/useAuth";
 export const ProtectedLayout = ({ children }: { children: JSX.Element }) => {
 	const auth = useAuth();
 
-	console.log(auth);
-
 	if (!auth.access) {
 		return <h1>VOCÊ NÃO ESTÁ LOGADO</h1>;
 	}
--- src/services/user/UserService.ts
+++ src/services/user/UserService.ts
@@ -1,5 +1,5 @@
 import axiosInstance from "../common/axiosInstance";
-import LoginResponse from "./types";
+import { LoginResponse, RegisterResponse } from "./types";
 
 class UserService {
 	async login(username: string, password: string): Promise<LoginResponse> {
@@ -9,12 +9,34 @@ class UserService {
 				password,
 			});
 
-			console.log("entrei");
-
 			if (response.status === 200) {
 				return response.data as LoginResponse;
 			} else {
 				throw new Error("Falha no login");
+			}
+		} catch (error) {
+			throw error;
+		}
+	}
+
+	async register(
+		nome: string,
+		username: string,
+		senha: string
+	): Promise<RegisterResponse> {
+		try {
+			const response = await axiosInstance.post("/cadastrar/", {
+				nome,
+				username,
+				senha,
+			});
+
+			console.log(response);
+
+			if (response.status === 201) {
+				return response.data as RegisterResponse;
+			} else {
+				throw new Error("Falha ao se cadastrar");
 			}
 		} catch (error) {
 			throw error;
